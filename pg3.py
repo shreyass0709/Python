6f5e20cbd9c94ec1b1566a8d6569621d49cc00c4
Shreyas S <shreyaspoojari292@gmail.com>
2026-06-06 23:47:18 +0530

    basics

---
+++ pg3.py
@@ -0,0 +1,32 @@
+x = 5
+y = "Hello world"
+print(x,y)
+print("Hello",end = " ")
+print("How are you?",end = " ");print("Bye bye!")
+print(3)
+print(3+3)
+print("my age is",20, "i am from kundapura")
+x = int(10)
+y = float(10)
+z = str(10)
+print(type(x))
+print(type(y))
+print(type(z))
+a = "hello"
+A = "world"
+print(a)
+print(A)
+my_name = "Shreyas"
+MyName = "Shreyas"
+myName = "Shreyas"
+
+x , y , z = 10, 20, 30
+print(x)    
+print(y)
+print(z)
+
+x = y = z = 20 
+print(x)
+print(y)
+print(z)
+
