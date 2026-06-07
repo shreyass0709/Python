782c981a6fa859aa166badb870b7b96e2aa3f78d
Shreyas S <shreyaspoojari292@gmail.com>
2026-06-07 18:32:16 +0530

    modify

---
+++ pg9.py
@@ -0,0 +1,7 @@
+a = " hello horld"
+
+print(a.upper())  # Converting the string to uppercase
+print(a.lower())  # Converting the string to lowercase
+print(a.strip())
+print(a.replace("h","j"))
+print(a.split("l"))
