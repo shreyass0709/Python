4d6d591af4c95f00f0ed744621538a5e785e4dd7
Shreyas S <shreyaspoojari292@gmail.com>
2026-06-07 19:01:39 +0530

    bool

---
+++ pg10.py
@@ -0,0 +1,15 @@
+a = "hello world"
+print(a.encode('utf-8'))  # Encoding the string to bytes using UTF-8 encoding
+
+c = 9
+d = 3
+
+print(c > d)  # Checking if c is greater than d
+
+e = 200
+b = 33
+
+if b > e:
+  print("b is greater than e")
+else:
+  print("b is not greater than e")
